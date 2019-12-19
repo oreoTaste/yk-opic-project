40760fd1f984c6cec65cbc2bb5fb9bfe89490c4f
YoungkukSohn <youngkuk.sohn@gmail.com>
2019-12-19 16:42:20 +0900

    (2019.12.19) v006 통합버전 업로드

--- src/main/java/yk/opic/App2.java
+++ src/main/java/yk/opic/App2.java
@@ -1,44 +1,90 @@
 package yk.opic;
 
+import java.sql.Date;
 import java.util.Scanner;
 
 public class App2 { 
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
     
-    System.out.print("번호? ");
-    int no = keyboard.nextInt();
-    keyboard.nextLine(); // 줄바꿈 기호 제거용
-    
-    System.out.print("이름? ");
-    String name = keyboard.nextLine();
-    
-    System.out.print("이메일? ");
-    String email = keyboard.nextLine();
-    
-    System.out.print("암호? ");
-    String password = keyboard.nextLine();
-    
-    System.out.print("사진? ");
-    String photo = keyboard.nextLine();
-    
-    System.out.print("전화? ");
-    String tel = keyboard.nextLine();
-    
-    System.out.print("가입일? ");
-    String registeredDate = keyboard.nextLine();
-    
-    keyboard.close();
-    
-    System.out.println();
-    
-    System.out.printf("번호: %d\n", no);
-    System.out.printf("이름: %s\n", name);
-    System.out.printf("이메일: %s\n", email);
-    System.out.printf("암호: %s\n", password);
-    System.out.printf("사진: %s\n", photo);
-    System.out.printf("전화: %s\n", tel);
-    System.out.printf("가입일: %s\n", registeredDate   );
-    
+    final int SIZE = 100;
+    Date[] registeredDate = new Date[SIZE];
+    int[] no = new int[SIZE];
+    String[] name = new String[SIZE];
+    String[] email = new String[SIZE];
+    String[] password = new String[SIZE];
+    String[] photo = new String[SIZE];
+    String[] tel = new String[SIZE];
+    int count = 0;
+
+    for(int i=0 ; ; i++) {
+      count++;
+      registeredDate[i] = new Date(System.currentTimeMillis());
+      System.out.print("번호? ");
+      no[i] = keyboard.nextInt();
+
+      keyboard.nextLine(); // nextInt() 후에 남아 있는 줄바꿈 기호를 제거한다.
+
+      System.out.print("이름? ");
+      name[i] = keyboard.nextLine();
+
+      System.out.print("이메일? ");
+      email[i] = keyboard.nextLine();
+
+      System.out.print("암호? ");
+      password[i] = keyboard.nextLine();
+
+      System.out.print("사진? ");
+      photo[i] = keyboard.nextLine();
+
+      System.out.print("전화? ");
+      tel[i] = keyboard.nextLine();
+
+      System.out.println();
+      System.out.print("계속 입력하시겠습니까?(Y/n) ");
+      String repeat = keyboard.nextLine();
+      System.out.println();
+      if(repeat.equals("N") || repeat.equals("n")) break;
+      else continue;
+    } keyboard.close();
+
+    for(int i=0 ; i<count ; i++){
+      System.out.printf("%d, %s , %s       , %s      , %tF\n",
+          no[i], name[i], email[i], tel[i], registeredDate[i]);
+    }
   }
 }
+/*
+
+번호? 1
+이름? 홍길동
+이메일? hong@test.com
+암호? 1111
+사진? hong.png
+전화? 1111-2222
+
+계속 입력하시겠습니까?(Y/n) y
+
+번호? 2
+이름? 임꺽정
+이메일? lim@test.com
+암호? 1111
+사진? lim.png
+전화? 1111-2223
+
+계속 입력하시겠습니까?(Y/n) y
+
+번호? 3
+이름? 전봉준
+이메일? jeon@test.com
+암호? 1111
+사진? jeon.png
+전화? 1111-2224
+
+계속 입력하시겠습니까?(Y/n) n
+
+1, 홍길동 , hong@test.com       , 1111-2222      , 2019-01-01
+2, 임꺽정 , lim@test.com        , 1111-2223      , 2019-01-01
+3, 전봉준 , jeon@test.com       , 1111-2224      , 2019-01-01
+
+*/
--- src/main/java/yk/opic/App3.java
+++ src/main/java/yk/opic/App3.java
@@ -5,31 +5,59 @@ import java.util.Scanner;
 
 public class App3 {
   public static void main(String[] args) {
-    Scanner keyboard = new Scanner(System.in);
-    
-    System.out.print("번호? ");
-    int no = keyboard.nextInt();
-    keyboard.nextLine(); // 줄바꿈 기호 제거용
-    
-    System.out.print("내용? ");
-    String title = keyboard.nextLine();
-    
-    // 현재 일시 
-    // currentTimeMillis()
-    // => 1970년 1월 1일 0시 0분 0초부터 현재까지 경과된 시간을 
-    //    밀리초로 리턴한다.
-    // new Date(밀리초)
-    // => 넘겨 받은 밀리초를 가지고 년,월,일,시,분,초를 계산한다.
-    Date today = new Date(System.currentTimeMillis());
+    java.io.InputStream inputStream = System.in;
+    Scanner scanner = new Scanner(inputStream);
+
+    final int SIZE = 100;
+    int[] no = new int [SIZE];
+    String[] title = new String [SIZE];
+    Date[] today = new Date[SIZE];
+    int[] pageOpened = new int[SIZE];
     int count = 0;
-    
-    keyboard.close();
-    
+
+    for(int i=0 ; ;i++) {
+      count++;
+      today[i] = new Date(System.currentTimeMillis());
+      pageOpened[i]++;
+      System.out.print("번호? ");
+      no[i] = scanner.nextInt();
+      scanner.nextLine(); // 줄바꿈 기호 제거용
+
+      System.out.print("내용? ");
+      title[i] = scanner.nextLine();
+      System.out.println();
+      System.out.print("계속 입력하시겠습니까?(Y/n) ");
+      String repeat = scanner.nextLine();
+      System.out.println();
+      if(repeat.equals("N") || repeat.equals("n")) break;
+      else continue;
+    } scanner.close();
+
+
     System.out.println();
-    
-    System.out.printf("번호: %d\n", no);
-    System.out.printf("내용: %s\n", title);
-    System.out.printf("작성일: %s\n", today);
-    System.out.printf("조회수: %d\n", count);
+    for(int i=0 ; i<count ; i++) {
+      System.out.printf("%d, %s              , %tF, %d\n",
+          no[i], title[i], today[i], pageOpened[i]);
+    }
   }
 }
+/*
+번호? 1
+내용? 게시글입니다.
+
+계속 입력하시겠습니까?(Y/n) y
+
+번호? 2
+내용? 두 번째 게시글입니다.
+
+계속 입력하시겠습니까?(Y/n) y
+
+번호? 3
+내용? 두 번째 게시글입니다.
+
+계속 입력하시겠습니까?(Y/n) n
+
+1, 게시글입니다.              , 2019-01-01, 0
+2, 두 번째 게시글입니다.        , 2019-01-01, 0
+3, 세 번째 게시글입니다.        , 2019-01-01, 0
+ */
