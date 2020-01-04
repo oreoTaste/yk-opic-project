ca0b5470c71595cbbe336b305f0866e8858afc1d
YoungkukSohn <youngkuk.sohn@gmail.com>
2020-01-04 19:14:10 +0900

    (2020.01.04) 코드수정(equals 메서드 오버라이드연습, 코드수정)

--- src/main/java/yk/learn/GetClassNInstanceof.java
+++ src/main/java/yk/learn/GetClassNInstanceof.java
@@ -4,7 +4,7 @@ class A1 {
   int a = 1;
   A1() {
     // new명령어로 instance생성시 출력
-    System.out.println("class A :" + a);
+    System.out.println("class A : " + a);
   }
 }
 
@@ -17,38 +17,48 @@ class B1 extends A1{
 }
 
 public class GetClassNInstanceof {
-  public static void main(StringClass[] args) {
+  public static void main(String[] args) {
+    
+    // Preparation
+    System.out.println("(((((preparation)))))");
     System.out.println("*** ObjA 생성 ***");
     A1 objA = new A1();
     System.out.println("*** ObjB 생성 ***");
     B1 objB = new B1();
     
     System.out.println();
-    System.out.println("the Use of instanceof");
+    
+    // Utilize Method of "instanceof"
+    System.out.println("(((((the Use of instanceof)))))");
+    //1.
     if(objA instanceof A1 ) {
       System.out.println("objA <=(instance)=> A");
       // A는 A의 인스턴스이거나 자손이다.(child class, sub class)
     }
+    //2.
     if(objA instanceof B1 ) {
       System.out.println("objA <=(instance)=> B");
       // B는 A의 인스턴스이거나 자손이다.(child class, sub class)
     }
-
+    //3.
     if(objB instanceof A1 ) {
       System.out.println("objB <=(instance)=> A");
       // B는 A의 인스턴스이거나 자손이다.(child class, sub class)
     }
-
+    //4.
     if(objB instanceof B1 ) {
       System.out.println("objB <=(instance)=> B");
       // B는 B의 인스턴스이거나 자손이다.(child class, sub class)
     }
     
+    // Utilize Method of "instanceof"
     System.out.println();
-    System.out.println("the Use of instanceof");
+    System.out.println("(((((the Use of getClass)))))");
+    //1.
     if(objA.getClass() == A1.class) {
       System.out.println("objA <=(getClass)=> A");
     }
+    //2.
     if(objA.getClass() == B1.class) {
       System.out.println("objA <=(getClass)=> B");
     }
@@ -57,8 +67,10 @@ public class GetClassNInstanceof {
       System.out.println("objB <=(getClass)=> A");
     }
     */
+    //3.
     if(objB.getClass() == B1.class) {
       System.out.println("objB <=(getClass)=> B");
     }
+    
   }
 }
--- src/main/java/yk/learn/OverloadingNOverriding.java
+++ src/main/java/yk/learn/OverloadingNOverriding.java
@@ -10,6 +10,12 @@ class A2 {
     this.a = overloading;
     System.out.println("A : " + overloading);
   }
+  
+  public boolean equals(Object object) {
+    if(object.getClass() != A2.class) return false;
+    if(this.a != ((A2)object).a) return false;
+    return true;
+  }
 }
 
 class B2 extends A2 {
@@ -36,5 +42,13 @@ public class OverloadingNOverriding {
     // 기존과 파라미터까지 모든 method signature가 동일하게 메서드를 변형.
     // 상속받은 클래스는 상속받은 메서드를 변형하여 메서드를 만든다.
     b2.print();
+    
+    
+    // Override Method of "equals"
+    System.out.println();
+    A2 a3 = new A2();
+    System.out.println("(((((Override Method of Equals)))))");
+    System.out.println(a3.equals(b2));
+    System.out.println(a3.equals(new A2()));
   }
 }
